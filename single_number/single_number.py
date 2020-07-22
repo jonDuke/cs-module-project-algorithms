7f230519a581e1c40b5db805e73aee0d8a3e9f1a
jonDuke <jonduke90@gmail.com>
2020-07-22 11:11:58 -0700

    completed single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,15 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    # sort arr so that all duplicate numbers will be next to each other
+    arr.sort()
+    for i in range(0, len(arr)-1, 2):
+        if arr[i] != arr[i+1]:
+            # didn't match, arr[i] is the single
+            return arr[i]
+    
+    # loop will not get to the last value if it is the single
+    return arr[-1]
 
 
 if __name__ == '__main__':

3b570a68bb5af8a09900f09c03a9db61ca6c6394
jonDuke <jonduke90@gmail.com>
2020-07-22 15:11:26 -0700

    completed rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  # Your code here
+  plays = ['rock', 'paper', 'scissors']
+  all_combos = []
+  find_combos(n, plays, all_combos)
+  return all_combos
 
-  pass
+def find_combos(n, plays, all_combos, combo=[]):
+  # Base case
+  if n == 0:
+    all_combos.append(combo)
+    return
+  
+  # Recursion: add each option and call again
+  for play in plays:
+    c = combo.copy()
+    c.append(play)
+    find_combos(n-1, plays, all_combos, c)
 
 
 if __name__ == "__main__":
